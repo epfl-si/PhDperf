118b03924f7cbc50786e41ef2a477c70d1298580
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2021-03-18 18:15:21 +0100

    [feature] Elementary navigation with react-router-dom
    Still missing: breadcrumbs

--- imports/ui/App.tsx
+++ imports/ui/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom"
+import { BrowserRouter as Router, Route, Switch, useParams } from "react-router-dom"
 import { Footer } from "epfl-sti-react-library"
 import { PhDHeader } from "./components/PhDHeader"
 import { PhDBreadcrumbs } from "./components/PhDBreadcrumbs"
 import { TaskList } from "./components/TaskList"
+import { Task } from "./components/Task"
 
 export const App = () => (
   <Router>
@@ -11,9 +12,21 @@ export const App = () => (
     <PhDBreadcrumbs />
     <div className="nav-toggle-layout nav-aside-layout">
       <div className="container">
-        <Route exact path="/" component={TaskList} />
+      <Switch>
+        <Route exact path="/">
+          <TaskList/>
+        </Route>
+        <Route path="/tasks/:key">
+          <TheTask/>
+        </Route>
+      </Switch>
       </div>
     </div>
     <Footer />
   </Router>
 )
+
+function TheTask() {
+  const { key } = useParams<{key: string}>()
+  return <Task workflowKey={key}/>
+}
---
+++ imports/ui/components/Task.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export function Task ({workflowKey} : {workflowKey : string}) {
+  return <h1>Task {workflowKey}</h1>
+}
