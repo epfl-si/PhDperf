e4a25b9a7b44ef5e426379005dfe5a68abdc920e
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2021-03-19 12:15:24 +0100

    [feature] Render form.io form from workflow header

--- imports/ui/components/Task.tsx
+++ imports/ui/components/Task.tsx
@@ -1,5 +1,19 @@
 import React from 'react'
+import { Form } from '@formio/react'
+import { PerfWorkflowTasks } from '/imports/ui/model/perf-workflow-tasks'
+import { Meteor } from 'meteor/meteor'
+import { useTracker } from 'meteor/react-meteor-data'
 
 export function Task ({workflowKey} : {workflowKey : string}) {
-  return <h1>Task {workflowKey}</h1>
+  useTracker(() => Meteor.subscribe('tasks'))
+  const task = useTracker(() => PerfWorkflowTasks.findByKey(workflowKey))
+  let formBody = <p>No form</p>
+  const formIoJson = task?.customHeaders?.form_io
+  if (formIoJson) {
+    formBody = <Form form={ JSON.parse(formIoJson as string) } />
+  }
+  return <>
+    <h1>Task {workflowKey}</h1>
+    {formBody}
+  </>
 }
--- imports/ui/model/perf-workflow-tasks.ts
+++ imports/ui/model/perf-workflow-tasks.ts
@@ -5,9 +5,18 @@ export type PerfWorkflowTask = PerfWorkflowTaskData & {
   getName : () => string
 }
 
-export const PerfWorkflowTasks = perfWorkflowTasksCollection<PerfWorkflowTask>((data) => {
+const PerfWorkflowTasks_ = perfWorkflowTasksCollection<PerfWorkflowTask>((data) => {
   const task = data as PerfWorkflowTask
   task.getUri = () => `/tasks/${data.key}`
   task.getName = () => JSON.stringify(data.variables)
   return task
 })
+
+const PerfWorkflowTasksClassMethods = {
+  findByKey(key : string) {
+    return PerfWorkflowTasks_.findOne({key})
+  }
+}
+
+export const PerfWorkflowTasks : typeof PerfWorkflowTasks_ & typeof PerfWorkflowTasksClassMethods =
+  Object.assign(PerfWorkflowTasks_, PerfWorkflowTasksClassMethods)
