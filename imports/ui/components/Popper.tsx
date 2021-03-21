70eaca7f66c0dc445b1d4a5703e06a836795f095
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2021-03-21 16:52:52 +0100

    [feature] Show/hide something when the little character is clicked

---
+++ imports/ui/components/Popper.tsx
@@ -0,0 +1,46 @@
+import React, { useRef, useEffect } from 'react'
+import { Options } from '@popperjs/core';
+import { usePopper } from 'react-popper'
+import _ from 'lodash'
+
+export function Popper(props: React.PropsWithChildren<{
+  visible?: boolean,
+  relativeTo: HTMLElement|null,
+  onClickElsewhere?: () => any,
+  options?: Omit<Partial<Options>, "modifiers">
+}>) {
+  const popperRef = useRef<HTMLDivElement>(null)
+
+  const { styles, attributes } = usePopper(
+    props.relativeTo,
+    popperRef.current,
+    props.options)
+
+  useEffect(() => {
+    if (! props.onClickElsewhere) return
+
+    const onClickElsewhere = props.onClickElsewhere
+
+    function handleDocumentClick(event : MouseEvent) {
+      const clickedWhere = event.target as Element
+      if (props.relativeTo?.contains(clickedWhere)) {
+        return
+      } else if (popperRef.current?.contains(clickedWhere)) {
+        return
+      } else {
+        onClickElsewhere()
+      }
+    }
+
+    document.addEventListener("mousedown", handleDocumentClick)
+    return /* destructor */ () => {
+      document.removeEventListener("mousedown", handleDocumentClick)
+    }
+  })
+
+  const style = _.extend({}, styles.popper, (props.visible && popperRef.current) ? {} : {display: 'none'})
+
+  return <div ref={popperRef} style={style} {...attributes.popper}>
+    {props.children}
+  </div>
+}
--- imports/ui/components/UserAuthButton.tsx
+++ imports/ui/components/UserAuthButton.tsx
@@ -1,12 +1,25 @@
 import { Meteor } from 'meteor/meteor'
-import React from 'react'
+import React, { useState, useRef } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { Become } from 'meteor/epfl:become'
 import { faUser, faUserClock, faUserShield } from '@fortawesome/free-solid-svg-icons'
+import { Popper } from './Popper'
 
 export function UserAuthButton() {
   const icon = (Meteor.user()?.isAdmin) ? faUserShield :
     Become.realUser() ? faUserClock : faUser
 
-  return <p><FontAwesomeIcon icon={icon} /></p>
+  const [visible, setVisibility] = useState<boolean>(false)
+  const buttonRef = useRef<HTMLButtonElement>(null)
+  const popperOptions = {
+    placement: "bottom-end",
+  }
+
+  return <>
+    <button ref={buttonRef} onClick={() => setVisibility(!visible)}><FontAwesomeIcon icon={icon} /></button>
+    <Popper relativeTo={buttonRef.current} options={popperOptions} visible={visible}
+            onClickElsewhere={() => setVisibility(false)}>
+    <p>Hello</p>
+    </Popper>
+  </>
 }
