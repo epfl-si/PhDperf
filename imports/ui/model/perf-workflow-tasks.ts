12873d428d7c788d950a4d3d423b5ecc35181f05
Julien Delasoie <julien.delasoie@epfl.ch>
2021-05-12 10:43:31 +0200

    Update urls for new cluster

--- imports/ui/model/perf-workflow-tasks.ts
+++ imports/ui/model/perf-workflow-tasks.ts
@@ -10,11 +10,11 @@ export type PerfWorkflowTask = FillFormTaskData & {
 
 const PerfWorkflowTasks_ = fillFormTasksCollection<PerfWorkflowTask>((data) => {
   const task = data as PerfWorkflowTask
-  const zeebeAddress = `be0628da-ddd4-4cfc-82ca-5c66504c7ef6` // should be process.env.ZEEBE_ADDRESS truncated but not in "no server parts"
+  const zeebeAddress = `fa5013db-2658-47ac-b6fc-497fc4b4757e` // should be process.env.ZEEBE_ADDRESS truncated but not in "no server parts"
   task.getUri = () => `/tasks/${data.key}`
   task.getName = () => `${data.customHeaders.title} (${data.key})`
   task.getDetail = () => ` version: ${data.workflowDefinitionVersion}, variables: ${JSON.stringify(_.omit(data.variables, 'metadata'), null, 2)}`
-  task.getOperateUri = () => `https://bru-1.operate.camunda.io/${zeebeAddress}/#/instances/${data.workflowInstanceKey}`
+  task.getOperateUri = () => `https://bru-2.operate.camunda.io/${zeebeAddress}/#/instances/${data.workflowInstanceKey}`
   return task
 })
 
