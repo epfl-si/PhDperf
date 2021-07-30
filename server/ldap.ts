6dfa3e13fa1454248a8357463261bc137c2ab74c
Julien Delasoie <julien.delasoie@epfl.ch>
2021-07-30 17:55:05 +0200

    Fix debug names

--- server/ldap.ts
+++ server/ldap.ts
@@ -1,9 +1,9 @@
 import debug_ from 'debug'
 import {LDAPUser} from "meteor/epfl:ldap";
 
-const debug = debug_('server/ldap')
-
-let ldapContext = require('epfl-ldap')({poolSize: 10});
+const debug = debug_('phdAssess:server:ldap')
+try {
+  let ldapContext = require('epfl-ldap')({poolSize: 10});
 
 export default function getUserBySciper(sciper: string, onSuccess: (user: LDAPUser) => void) {
   debug(`asking to fetch LDAP for ${sciper}`)
