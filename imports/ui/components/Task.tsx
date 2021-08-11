acf038d30aa10054fa1d0e27dffbbc8025cb9325
Julien Delasoie <julien.delasoie@epfl.ch>
2021-08-11 13:36:43 +0200

    Better error management for a task

--- imports/ui/components/Task.tsx
+++ imports/ui/components/Task.tsx
@@ -1,16 +1,14 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {Errors, Form} from '@formio/react'
 import {customEvent} from '/imports/ui/model/formIo'
 import {PerfWorkflowTasks} from '/imports/ui/model/perf-workflow-tasks'
-import {Meteor} from 'meteor/meteor'
+import {global_Error, Meteor} from 'meteor/meteor'
 import {useTracker} from 'meteor/react-meteor-data'
-import {Loader} from "epfl-sti-react-library"
-/*
 import {Alert, Button, Loader} from "epfl-sti-react-library"
 import {Link} from "react-router-dom"
-*/
 import _ from "lodash";
 import findDisabledFields from "/imports/lib/formIOUtils";
+import { useHistory } from "react-router-dom";
 
 export function Task({workflowKey}: { workflowKey: string }) {
   const taskLoading = useTracker(() => {
@@ -20,35 +18,45 @@ export function Task({workflowKey}: { workflowKey: string }) {
     return !handle.ready();
   }, [workflowKey]);
 
-  const task = useTracker(() => PerfWorkflowTasks.findByKey(workflowKey), [workflowKey])
+  const task = PerfWorkflowTasks.findByKey(workflowKey)
+
+  const [submitting, setSubmitting] = useState(false)
+  const [submitted, setSubmitted] = useState(false)
+  const [meteorError, setMeteorError] = useState("")
+
+  const history = useHistory()
 
   return (
     <>
       {
+        !submitted ?
         task?.formIO ?
-        taskLoading ? (<>
-          <Loader message={'Fetching task...'}/>
-        </>) : (<>
-          <h1 className={'h2'}>{task.title || `Task ${workflowKey}`}</h1>
-          <Errors/>
-          <Form form={JSON.parse(task.formIO)}
-                submission={ { data: task.variables }}
-                noDefaultSubmitButton={true}
-                onSubmit={onSubmit}
-                onCustomEvent={(event: customEvent) => event.type == 'cancelClicked' && window.history.go(-1)}
-          />
-        </>)
-      :
-      (<div>Unable to find the requested task no {workflowKey}</div>)
+          taskLoading ? (<>
+              <Loader message={'Fetching task...'}/>
+            </>) :
+            submitting ? <div>Submitting...</div>
+              :
+              (<>
+                <h1 className={'h2'}>{task.title || `Task ${workflowKey}`}</h1>
+                <Errors/>
+                <Form form={JSON.parse(task.formIO)}
+                      submission={{data: task.variables}}
+                      noDefaultSubmitButton={true}
+                      onSubmit={onSubmit}
+                      onCustomEvent={(event: customEvent) => event.type == 'cancelClicked' && history.push('/')}
+                />
+              </>)
+            :
+            <div>Unable to find the requested task no {workflowKey}</div>
+          :
+            !meteorError ?
+            <>
+              <Alert message={'Form submitted'} alertType={'success'}/>
+              <Link to={`/`}><Button label={'Back'} onClickFn={() => void 0}/></Link>
+            </>
+            : <Alert message={`Form not submitted because of an error ${meteorError}. Please retry again`} alertType={'danger'}/>
       }
     </>)
-
-  /*
-  (<>
-          <Alert message={'Form submitted'} alertType={'success'}/>
-          <Link to={`/`}><Button label={'Back'} onClickFn={() => void 0}/></Link>
-        </>)
-   */
 
   function onSubmit(this: any, formData: { data: any, metadata: any }) {
     // As formio sent all the form fields (disabled included)
@@ -56,10 +64,19 @@ export function Task({workflowKey}: { workflowKey: string }) {
     // the workflow variables with only the needed values
     const formDataPicked = _.omit(formData.data, findDisabledFields(this.form))
 
+    setSubmitting(true)
+    debugger
     Meteor.call("submit",
       workflowKey,
       formDataPicked,
-      formData.metadata
+      formData.metadata,
+      (error: global_Error | Meteor.Error | undefined) => {
+        setSubmitting(false)
+        setSubmitted(true)
+        if (error) {
+          setMeteorError(error.message)
+        }
+      }
     )
   }
 }
