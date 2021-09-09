adfebfb177146b9c5a443b5552faf19ce79f11c7
Julien Delasoie <julien.delasoie@epfl.ch>
2021-09-09 15:01:24 +0200

    Change the way we fetch epfl-sti-react library

--- imports/ui/components/PhDHeader.tsx
+++ imports/ui/components/PhDHeader.tsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { Drawer, Logo } from "epfl-sti-react-library"
+import { Drawer, Logo } from "@epfl/epfl-sti-react-library"
 import { UserAuthButton } from "./UserAuthButton"
 
 export function PhDHeader() {
