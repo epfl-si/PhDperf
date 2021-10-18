a20eb9018d77189529ae87037b570a30760b748c
Julien Delasoie <julien.delasoie@epfl.ch>
2021-10-18 15:42:01 +0200

    Improve task listing for the cases where variables are missing

--- imports/ui/components/TaskList.tsx
+++ imports/ui/components/TaskList.tsx
@@ -26,9 +26,16 @@ function Task({task}: TaskProps) {
       <details>
         <summary className={'d-flex align-items-center'}>
           <span className={'mr-auto small'}>
-            <span className={'mr-1'}>Created by {task.variables.created_by}</span>
-            <span className={'mr-1'}>Created {task.created_at?.toLocaleString('fr-CH')}</span>
-            <span>Updated {task.updated_at?.toLocaleString('fr-CH')}</span>
+            {task.variables &&
+              task.variables.created_by &&
+                <span className={'mr-1'}>Created by {task.variables.created_by}</span>
+            }
+            {task.created_at &&
+              <span className={'mr-1'}>Created {task.created_at.toLocaleString('fr-CH')}</span>
+            }
+            {task.updated_at &&
+              <span>Updated {task.updated_at.toLocaleString('fr-CH')}</span>
+            }
           </span>
           <span className={'small'}>
             { task.monitorUri &&
