d0a65cb02d487e6fbb34c277f106da5dc9f0c8b6
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2021-11-02 15:30:37 +0100

    [feature] Function `getDiskUsage()`

--- docker/monitoring/server.ts
+++ docker/monitoring/server.ts
@@ -3,6 +3,7 @@
 import express from 'express'
 import * as prom from 'prom-client'
 import dotenv from 'dotenv'
+import { spawn } from 'child_process'
 
 dotenv.config()
 
@@ -18,6 +19,8 @@ setInterval(async function() {
     diskUsageRunning = true
     try {
       await measureDiskUsage()
+    } catch (e) {
+      console.error(e)
     } finally {
       diskUsageRunning = false
     }
@@ -25,8 +28,30 @@ setInterval(async function() {
 }, 1000 * Number(process.env.DISK_USAGE_PERIOD_SECONDS || 10))
 
 async function measureDiskUsage() {
-  console.log("coucou")
-  await new Promise((resolve) => setTimeout(resolve, 5000))
+  const usage = await getDiskUsage(process.env.DISK_USAGE_TARGET || ".")
+  console.log(usage)
+}
+
+async function getDiskUsage(path : string) {
+  const out : Array<Buffer> = []
+  await new Promise<void>((resolve, reject) => {
+    const du = spawn("du", ["-s", path],
+                     {stdio: ['ignore', 'pipe', 'inherit']})
+    du.on("error", reject)
+
+    du.stdout.on("data", (data : string) => { out.push(Buffer.from(data)) })
+
+    du.on("close", (code : Number) => {
+      if (code === 0) {
+        resolve()
+      } else {
+        reject(`du exited with code ${code}`)
+      }
+    })
+  })
+
+  const stdout = Buffer.concat(out).toString("ascii")
+  return stdout ? Number(stdout.match(/^\d+/)[0]) : undefined
 }
 
 /////////////////////////////////////////////////////////////////////////////////////
