04bf820e66d7e5af71d15d48feb3148c0b06c17b
Julien Delasoie <julien.delasoie@epfl.ch>
2022-01-28 17:04:09 +0100

    Fix wrong Typescript var existence

--- imports/policy/importScipers.tsx
+++ imports/policy/importScipers.tsx
@@ -1,5 +1,5 @@
 import {Meteor} from "meteor/meteor";
 
 export const canImportScipersFromISA = () : boolean => {
-  return !!(Meteor.user()!.isAdmin || Meteor.user()?.isProgramAssistant)
+  return !!(Meteor.user()?.isAdmin || Meteor.user()?.isProgramAssistant)
 }
