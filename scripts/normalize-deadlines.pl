d69287ace455569bdb578c2d437e13171143cc03
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2022-03-22 12:30:12 +0100

    [feature] Script to delete and recreate the JOB_DEADLINES column family
    Should help with [OUT0006962](https://epfl.service-now.com/nav_to.do?uri=cmdb_ci_outage.do?sys_id=f883c7451b52cd9056c1ebd9bb4bcb8a)

---
+++ scripts/normalize-deadlines.pl
@@ -0,0 +1,110 @@
+#!/usr/bin/env perl
+
+=head1 NAME
+
+C<normalize-deadlines.pl> - Refresh the C<JOB_DEADLINES> column family from the ground truth in C<JOBS>
+
+=head1 USAGE
+
+  normalize-deadlines.pl <snapshotdir>
+
+💡 If the C<RocksDB> Perl module (whose build is a bit of an
+adventure) is unavailable, the script automatically re-runs itself
+through the Docker image built from C<../docker/zeebe-tools>.
+
+=cut
+
+use FindBin qw($Bin $Script);
+use File::Spec;
+# For ZeebeDB::Key while outside the container - Doesn't harm inside it:
+use lib "$Bin/perllib";
+
+# If we can't have RocksDB, run the same script inside Docker:
+BEGIN {
+  eval {
+    require RocksDB;
+  } or do {
+    my $docker_build_dir = "$Bin/../docker/zeebe-tools";
+    system("docker", "build", "-t", "epfl-si/zeebe-tools",
+           $docker_build_dir)
+      and die "Unable to build Docker image in $docker_build_dir (exit code $?)";
+    my $rocksdb_dir = File::Spec->rel2abs(pop @ARGV);
+    exec("docker", "run", "--rm",
+         "-v", "$Bin/$Script:/$Script:ro",
+         "-v", "$Bin/perllib:/usr/local/lib/site_perl:ro",
+         "-v", "$rocksdb_dir:/rocksdb",
+         "epfl-si/zeebe-tools",
+         "perl", "/$Script", @ARGV, "/rocksdb");
+  };
+}
+
+use v5.21;
+use Data::MessagePack;
+use ZeebeDB::Key;
+
+my $db = RocksDB->new($ARGV[0]);
+
+my %job_deadlines;
+
+sub walk_column_family ($&);
+
+warn "Scanning JOB_STATES...\n";
+walk_column_family JOB_STATES => sub {
+  my (undef, $state, undef, $jobKey) = @_;
+  $jobKey = $jobKey->pretty;
+  if ($state->{jobState} eq "ACTIVATED") {
+    $job_deadlines{$jobKey} = 1;
+  }
+};
+warn ("... " . keys(%job_deadlines) . " activated jobs found.\n");
+
+warn "Scanning JOBS...\n";
+walk_column_family JOBS => sub {
+  my (undef, $job, $cf, $jobKey) = @_;
+  $jobKey = $jobKey->pretty;
+  next unless $job_deadlines{$jobKey} and (my $deadline = $job->{jobRecord}->{deadline});
+  $job_deadlines{$jobKey} = {
+    timestamp => $deadline,
+    key => [
+      ZeebeDB::Key::Tok::Timestamp->new($deadline),
+      ZeebeDB::Key::Tok::ZeebeKey->new($jobKey)
+    ]
+  };
+};
+
+my $orphan_count = 0;
+foreach my $j (keys %job_deadlines) {
+  if ($job_deadlines{$j} == 1) {
+    $orphan_count++;
+    delete $job_deadlines{$j};
+  }
+}
+warn ("... found $orphan_count jobs without a deadline.\n");
+
+warn "Deleting all JOB_DEADLINES...\n";
+my $deleted_count = 0;
+walk_column_family JOB_DEADLINES => sub {
+  my ($key) = @_;
+  $db->delete($key);
+  $deleted_count++;
+};
+warn "... $deleted_count entries deleted.\n";
+
+warn "Populating JOB_DEADLINES...\n";
+my $deadline_cf = ZeebeDB::Key::Tok::ColumnFamily->new("JOB_DEADLINES");
+my $nil = Data::MessagePack->pack(-1);
+foreach my $deadline (sort { $a->{timestamp} <=> $b->{timestamp}} (values %job_deadlines)) {
+  $db->put(ZeebeDB::Key->serialize($deadline_cf, @{$deadline->{key}}), $nil)
+}
+warn "... " . values(%job_deadlines) . " entries added.\n";
+
+sub walk_column_family ($&) {
+  my ($cf_name, $walker) = @_;
+  my $iter = $db->new_iterator->seek_to_first;
+  while (my ($key, $value) = $iter->each) {
+    eval { $value = Data::MessagePack->unpack($value) };
+    my ($cf, @key_toks) = ZeebeDB::Key->parse($key);
+    next unless $cf->name eq $cf_name;
+    $walker->($key, $value, $cf, @key_toks);
+  }
+}
