6c6c68e1974e9ad68826183a779fb24ca5bcaca8
Julien Delasoie <julien.delasoie@epfl.ch>
2022-04-26 16:08:53 +0200

    Update accounts-tequila, as needed for the new Tequila 2.1 protocol

--- @types/meteor/epfl:accounts-tequila/index.d.ts
+++ @types/meteor/epfl:accounts-tequila/index.d.ts
@@ -3,6 +3,7 @@
 declare module 'meteor/epfl:accounts-tequila' {
     namespace Tequila {
         function start(options?: any): void
+        const defaultOptions: any
     }
     export default Tequila
 }
