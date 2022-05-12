eea72ac18347a20a41600eb0b8f957f37e4af7d8
Dominique Quatravaux <dominique.quatravaux@epfl.ch>
2022-05-12 14:58:03 +0200

    [refactor] Introduce functions

--- tools/tasks_journal.R
+++ tools/tasks_journal.R
@@ -4,15 +4,22 @@ library(lubridate)
 library(glue)
 library(ggplot2)
 
-tasks_journal <-
+get_tasks_journal_cmd <- function(namespace) {
     glue::glue(r"(
-       oc -n phd-assess exec -i services/mongo -- \
+       oc -n { namespace } exec -i services/mongo -- \
           mongo --quiet meteor --eval "printjson(db.tasks_journal.find().toArray());" \
        | grep -v '"msg"' | sed -e 's/ISODate(\(".*"\))/\1/' | jq -c '.[]'
-    )") %>%
-    pipe() %>%
-    stream_in() %>%
-    mutate(lastSeen = ymd_hms(lastSeen))
+    )")
+}
+
+get_tasks_journal <- function(namespace) {
+    get_tasks_journal_cmd(namespace) %>%
+        pipe() %>%
+        stream_in() %>%
+        mutate(lastSeen = ymd_hms(lastSeen))
+}
+
+tasks_journal <- get_tasks_journal(namespace = "phd-assess")
 
 tasks_journal %>%
     ggplot(aes(x = lastSeen, y = seenCount)) +
