a3ff3f5a2cd6efb35c7340fb7963a8c325fd3abf
Julien Delasoie <julien.delasoie@epfl.ch>
2022-12-05 14:14:57 +0100

    Improve Asidemenu code layout

--- imports/ui/components/AsideMenu.tsx
+++ imports/ui/components/AsideMenu.tsx
@@ -1,6 +1,7 @@
 import {Link, matchPath, useLocation} from "react-router-dom";
 import React from "react";
-import {useSubscribe} from "meteor/react-meteor-data";
+
+import {Loader} from "@epfl/epfl-sti-react-library";
 
 import {useAccountContext} from "/imports/ui/contexts/Account";
 import {canEditAtLeastOneDoctoralSchool} from "/imports/policy/doctoralSchools";
@@ -10,39 +11,44 @@ import {canImportScipersFromISA} from "/imports/policy/importScipers";
 export const AsideMenu = () => {
   const account = useAccountContext()
   const { pathname } = useLocation()
-  useSubscribe('doctoralSchools');
 
   return (
     <aside className="nav-aside-wrapper">
       <nav id="nav-aside" className="nav-aside" role="navigation" aria-describedby="nav-aside-title">
-        <ul>
-          { account && account.user && canEditAtLeastOneDoctoralSchool(account.user) &&
+        { (!account || !account.user) ?
+          <Loader/> :
+          <ul>
+            { canEditAtLeastOneDoctoralSchool(account.user) &&
+              <li>
+                <a href="#">Doctoral Programs</a>
+                <ul>
+                  <li className={matchPath("/doctoral-programs", pathname) ? 'active' : undefined}>
+                    <Link to={`/doctoral-programs`}>Administration</Link>
+                  </li>
+                </ul>
+              </li>
+            }
             <li>
               <a href="#">
-                Doctoral Programs
+                Tasks
               </a>
               <ul>
-                <li className={matchPath("/doctoral-programs", pathname) ? 'active' : undefined}><Link
-                  to={`/doctoral-programs`}>Administration</Link></li>
+                <li className={matchPath("/", pathname) ? 'active' : undefined}>
+                  <Link to={`/`}>List</Link>
+                </li>
+                <li className={matchPath("/dashboard", pathname) ? 'active' : undefined}>
+                  <Link to={`/dashboard`}>Dashboard</Link>
+                </li>
+                { canImportScipersFromISA(account.user) &&
+                  <li className={matchPath("/import-scipers", pathname) ? 'active' : undefined}>
+                    <Link to={`/import-scipers`}>Import scipers</Link>
+                  </li>
+                }
               </ul>
             </li>
-          }
-          <li>
-            <a href="#">
-              Tasks
-            </a>
-            <ul>
-              <li className={matchPath("/", pathname) ? 'active' : undefined }><Link to={`/`}>List</Link></li>
-              <li className={matchPath("/dashboard", pathname) ? 'active' : undefined}><Link
-                to={`/dashboard`}>Dashboard</Link></li>
-                { account && account.user && canImportScipersFromISA(account.user) &&
-                  <li className={matchPath("/import-scipers", pathname) ? 'active' : undefined }><Link
-                    to={`/import-scipers`}>Import scipers</Link></li>
-                }
-            </ul>
-          </li>
-        </ul>
-    </nav>
+          </ul>
+        }
+      </nav>
     </aside>
   )
 }
