fc3ac2d18ab1c215ae40de99d8b7e45feaa6b881
Julien Delasoie <julien.delasoie@epfl.ch>
2022-12-05 13:34:47 +0100

    Improve userauthinfo code

--- imports/ui/components/UserAuthInfo.tsx
+++ imports/ui/components/UserAuthInfo.tsx
@@ -2,6 +2,8 @@ import React, { useState, useRef } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUser, faUserShield } from '@fortawesome/free-solid-svg-icons'
 
+import {Loader} from "@epfl/epfl-sti-react-library";
+
 import packageJson from '/package.json'
 import {useAccountContext} from "/imports/ui/contexts/Account";
 import { Popper, PopperOptions } from './Popper'
@@ -9,9 +11,6 @@ import { Popper, PopperOptions } from './Popper'
 
 export const UserAuthInfo = () => {
   const account = useAccountContext()
-  const user = account?.user
-
-  const icon = ( user?.isAdmin) ? faUserShield : faUser
 
   const [visible, setVisibility] = useState<boolean>(false)
   const buttonRef = useRef<HTMLButtonElement>(null)
@@ -27,20 +26,22 @@ export const UserAuthInfo = () => {
       ]
   }
 
+  if (!account || !account.user) return (<Loader></Loader>)
+
   return <>
     <button ref={buttonRef} onClick={() => setVisibility(!visible)}
             className="btn btn-secondary dropdown-toggle">
-      <FontAwesomeIcon icon={icon} />
+      <FontAwesomeIcon icon={ account.user.isAdmin ? faUserShield : faUser } />
     </button>
     <Popper relativeTo={buttonRef.current} options={popperOptions} visible={visible}
             onClickElsewhere={() => setVisibility(false)}>
     <div className="dropdown-menu-popper mt-3">
       <div className={'mx-3 my-3'}>
         <div>
-          Logged in as { user?.tequila?.displayname }
-          { user?.isAdmin && ` (admin)` }
+          Logged in as { account.user.tequila?.displayname }
+          { account.user.isAdmin && ` (admin)` }
         </div>
-        { user?.isAdmin &&
+        { account.user.isAdmin &&
         <div className={'small'}>
           Version {packageJson.version}
         </div>
