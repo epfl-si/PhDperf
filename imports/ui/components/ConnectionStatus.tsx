b1c517e46a9bd5d02e6d62cd0c8139fa9760b69d
Julien Delasoie <julien.delasoie@epfl.ch>
2022-12-07 10:55:50 +0100

    Allow to control client connection trough the UI

--- imports/ui/components/ConnectionStatus.tsx
+++ imports/ui/components/ConnectionStatus.tsx
@@ -5,18 +5,32 @@ import {zeebeStatusCollection} from "/imports/ui/model/zeebeStatus";
 
 
 /**
+ * Allow to control the client connexion.
+ */
+const ClientConnectionController = () => {
+  const status = useTracker(() => {
+    return Meteor.status()
+  })
+
+  return (
+    <div className={'mx-2'}>
+      <span>Client status:</span>&nbsp;
+      <span className="">
+          { JSON.stringify(status, null, ' ') }
+        </span>
+      <button className={'mx-1'} onClick={() => Meteor.reconnect()}>Reconnect</button>
+      <button className={'mx-1'} onClick={() => Meteor.disconnect()}>Disconnect</button>
+    </div>
+  )
+}
+
+/**
  * Here are shown information coming from
  *   1. the  client connection to Meteor (aka Meteor.status())
  *   2. the server connection to Zeebe (crawled from the zeebe.status table, updated by the server)
  * If one or another fail, it may be bad, so we notify it
  */
 export function ConnectionStatusFooter() {
-  /*
-  const status = useTracker(() => {
-    return Meteor.status()
-  })
-  */
-
   const zeebeStatusLoading = useTracker(() => {
     const handle = Meteor.subscribe('zeebe.status')
     return !handle.ready()
@@ -30,18 +44,6 @@ export function ConnectionStatusFooter() {
 
   return (
     <div className={'d-flex flex-row-reverse small mr-3 mt-4'}>
-
-      { /* Commenting until needed by devs
-      <div className={'pl-1'}>
-        <span>Client status:</span>&nbsp;
-        <span className="font-weight-bold">
-          { status.status }
-        </span>
-        <button onClick={() => Meteor.reconnect()}>Reconnect</button>
-        <button onClick={() => Meteor.disconnect()}>Disconnect</button>
-      </div>
-      */ }
-
       <div className={'pl-1'}>
         <span>Server status:</span>&nbsp;
         <span className="font-weight-bold">
@@ -53,6 +55,9 @@ export function ConnectionStatusFooter() {
           }
         </span>
       </div>
+      { Meteor.settings.public.showClientConnexionControl &&
+        <ClientConnectionController/>
+      }
     </div>
   )
 }
