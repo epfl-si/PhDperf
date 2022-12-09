42983c09c1b09d80de99195ee975b865cbe0e874
Julien Delasoie <julien.delasoie@epfl.ch>
2022-12-09 15:57:39 +0100

    Remove unused route loader

--- imports/ui/routes.tsx
+++ imports/ui/routes.tsx
@@ -15,7 +15,6 @@ import React from "react";
 
 import {TaskForm} from "/imports/ui/components/TaskForm";
 import Main from "/imports/ui/Main";
-import {Meteor} from "meteor/meteor";
 
 
 function TaskEdit() {
@@ -32,23 +31,8 @@ export const router = createBrowserRouter(
     >
       <Route index element={<TaskList />} />
       <Route path="/dashboard" element={<Dashboard/>}/>
-      <Route
-        path="/tasks/:_id"
-        element={<TaskEdit/>}
-        loader={
-          async ({ params }) => {
-            // @ts-ignore
-            const task = await Meteor.applyAsync(
-              'getTaskForm',
-              [params._id],
-              { wait: true }
-            )
-
-            return task ?? null
-          }
-        }
-      />
-      <Route path="/tasks/" element={<Navigate replace to="/" />} />
+      <Route path="/tasks/:_id" element={<TaskEdit/>}/>
+      <Route path="/tasks/" element={<Navigate replace to="/" />}/>
       <Route path="/doctoral-programs" element={<DoctoralSchoolsList/>}/>
       <Route path="/import-scipers/:doctoralSchool" element={<ImportScipersForSchool/>}/>
       <Route path="/import-scipers/" element={<ImportScipersSchoolSelector/>} />
