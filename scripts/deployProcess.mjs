a65a8df0a7bb59430f054801fb6f5cf9f59935d6
Julien Delasoie <julien.delasoie@epfl.ch>
2023-03-20 12:34:44 +0100

    Modernize the script with zx

--- scripts/deployProcess.mjs
+++ scripts/deployProcess.mjs
@@ -1,13 +1,27 @@
-//const findUp = require('find-up')
-import {findUpSync} from 'find-up'
-import ZB from 'zeebe-node'
-import dotenv from 'dotenv'
+#!/usr/bin/env zx
+/*
+  * zx scripts https://github.com/google/zx
+  * prerequiste :
+  * npm i -g zx
+  * npm i -g zbctl
+*/
 
-dotenv.config({path: findUpSync(".env")})
+import * as fs from 'fs'
+import * as os from 'os'
+import * as path from 'path'
 
-void (async () => {
-  const zbc = new ZB.ZBClient() // localhost:26500 || ZEEBE_GATEWAY_ADDRESS
+const bpmnURL = 'https://raw.githubusercontent.com/epfl-si/PhDAssess-meta/main/bpmn/phdAssessProcess.bpmn'
+const bpmnFullPath = path.join(os.tmpdir(), 'phdAssessProcess.bpmn')
 
-  const res = await zbc.deployProcess('bpmn-model/phdAssessProcess.bpmn')
-  console.log(res)
-})()
+let zeebePort = await question('What port is your zeebe instance running on ? [26501] ')
+if (!zeebePort) zeebePort = '26501'
+
+fetch(bpmnURL).then(async res => {
+  const dest = fs.createWriteStream(bpmnFullPath)
+  await res.body.pipe(dest)
+  console.log('File downloaded successfully')
+
+  $`zbctl deploy --port ${ zeebePort } --insecure ${ bpmnFullPath };`
+  console.log('BPMN deployed successfully')
+
+}).catch(err => console.error(err))
