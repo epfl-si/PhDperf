4bb66f904dfc1dc29556d234e867a6628dd5bb2c
Julien Delasoie <julien.delasoie@epfl.ch>
2023-04-12 10:50:02 +0200

    Add tests about task dates

--- apps/fillForm/tests/server/unitTest.ts
+++ apps/fillForm/tests/server/unitTest.ts
@@ -1,18 +1,23 @@
+import chai, {assert} from 'chai'
+import chaiDateTime from "chai-datetime";
+chai.use(chaiDateTime);
+
 import {Meteor} from "meteor/meteor";
+
 import {Tasks} from "/imports/model/tasks";
-import assert from "assert";
 import {DoctoralSchools} from "/imports/api/doctoralSchools/schema";
 import {initialEcolesDoctorales} from "/server/fixtures/doctoralSchools";
+import dayjs from "dayjs";
 
 const dbCleaner = require("meteor/xolvio:cleaner");
-const _dburlesFactory = require("meteor/dburles:factory");
+const Factory = require("meteor/dburles:factory").Factory
 
 
 describe('Unit tests Tasks', function () {
   beforeEach(function () {
     dbCleaner.resetDatabase({}, () => {
-      _dburlesFactory.Factory.create("task");
-      _dburlesFactory.Factory.create("task");
+      Factory.create("task");
+      Factory.create("task");
     });
   });
 
@@ -25,6 +30,34 @@ describe('Unit tests Tasks', function () {
     //   console.log(taskLight)
     // })
     });
+
+  // testing the test engine about dates, and how we are able to filter it with Mongo
+  it('should be able to read and write dates', function () {
+    Factory.create("task", {
+        "journal.lastSeen": dayjs().subtract(15, 'days').toISOString(),
+      }
+    )
+
+    const tasks = Tasks.find({}).fetch()
+    assert.isNotEmpty(tasks)
+
+    tasks.forEach((task) => {
+      assert.isDefined(task.journal.lastSeen)
+      const lastSeen = new Date(task.journal.lastSeen!)
+      assert.beforeOrEqualDate(lastSeen, new Date())
+    })
+
+    // can we find the obsolete one ?
+    const obsoleteTasks = Tasks.find({
+      "journal.lastSeen": { $lte: dayjs().subtract(1, 'day').toISOString() },
+    }).fetch()
+    const notObsoleteTasks = Tasks.find({
+      "journal.lastSeen": { $gte: dayjs().subtract(1, 'day').toISOString() },
+    }).fetch()
+
+    assert.lengthOf(obsoleteTasks, 1)
+    assert.isAbove(notObsoleteTasks.length, 1)
+  });
 });
 
 describe('Unit tests DoctoralSchools', function () {
