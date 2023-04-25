8abcd317e920c41495d7915d76820da4713260aa
Julien Delasoie <julien.delasoie@epfl.ch>
2023-04-25 15:24:44 +0200

    Add zx script as a shortcut to use the snapshot reader

--- zx.mjs
+++ zx.mjs
@@ -15,6 +15,8 @@ if (argv.help || argv._[0] === 'help') {
   await clean(...argv._.slice(1));
 } else if (argv._[0] === 'deploy-bpmn') {
   await deployProcess();
+} else if (argv._[0] === 'read-snapshot') {
+  await readSnapshot(...argv._.slice(1));
 } else {
   await help(...argv._);
 }
@@ -28,4 +30,27 @@ async function run(args) {
 }
 
 async function clean(args) {
+}
+
+async function readSnapshot(args) {
+  if (await question('Do you want to start the docker build of the zeebe_tools first ? [y/N] ') === 'y') {
+    const dockerBuild = await $`docker build -t zeebe-tools docker/zeebe-tools`
+    //console.log(dockerBuild)
+    for await (const chunk of dockerBuild.stderr) {
+      echo(chalk.yellow(dockerBuild))
+    }
+  }
+
+  const snapshotPath = await question('Path to snapshot ? (should have the CURRENT file inside) ')
+  const outputFilePath = await question('Full path of the destination file: ')
+
+  if (!outputFilePath) {
+    console.log('Aborting, no path were given for the output file')
+  } else {
+    await spinner(
+      'decompressing the snapshot...', () =>
+      $`docker run -v $PWD/scripts:/scripts -v $PWD/docker/zeebe-tools/perllib:/docker/zeebe-tools/perllib -v ${snapshotPath}:/snapshot zeebe-tools perl /scripts/read-snapshot.pl /snapshot > ${outputFilePath}`
+    )
+    console.log('Done.')
+  }
 }
