97fd981e665ec0790078274d8a23b56ea33aab02
Julien Delasoie <julien.delasoie@epfl.ch>
2023-08-17 11:22:27 +0200

    test:  Refactor how to create a specific activity for a task, easing the creation of one multiple activities for one instance

--- apps/fillForm/tests/factories/dashboard/tasksV1.ts
+++ apps/fillForm/tests/factories/dashboard/tasksV1.ts
@@ -1,211 +1,187 @@
 /**
- * Define tasks to expose explicitily steps in the dashboard
+ * Define tasks to expose explicitly steps in the dashboard
  */
-
 import {faker} from "../../factories/faker";
 const Factory = require("meteor/dburles:factory").Factory
 import {generateAGenericTaskAttributes} from "../task";
 
 
-export const getPHDFills = () => {
-  const taskAttributes = generateAGenericTaskAttributes();
-  return {
-    ...taskAttributes,
-    'customHeaders': {
-      ...taskAttributes.variables,
-      'title': 'Phd fills annual report - Test',
-    },
-    'variables': {
-      ...taskAttributes.variables,
-      'assigneeSciper': taskAttributes.variables.thesisCoDirectorSciper,
-      'dashboardDefinition': undefined,
-    },
-    'elementId': faker.helpers.arrayElement([
-      'Activity_PHD_fills_annual_report',
-    ]),
-  }
-}
+export const setPHDFillsAttributes = (taskAttributes: any = generateAGenericTaskAttributes()) => ({
+  ...taskAttributes,
+  'customHeaders': {
+    ...taskAttributes.variables,
+    'title': 'Phd fills annual report - Test',
+  },
+  'variables': {
+    ...taskAttributes.variables,
+    'assigneeSciper': taskAttributes.variables!.thesisCoDirectorSciper,
+    'dashboardDefinition': undefined,
+  },
+  'elementId': faker.helpers.arrayElement([
+    'Activity_PHD_fills_annual_report',
+  ]),
+})
 
-export const getCoDirectorFillsAttributes = () => {
-  const taskAttributes = generateAGenericTaskAttributes();
-  return {
-    ...taskAttributes,
-    'customHeaders': {
-      ...taskAttributes.variables,
-      'title': 'Co-Dir fills annual report - Test',
-    },
-    'variables': {
-      ...taskAttributes.variables,
-      'assigneeSciper': taskAttributes.variables.thesisCoDirectorSciper,
-      'dashboardDefinition': undefined,
-    },
-    'elementId': faker.helpers.arrayElement([
-      'Activity_Thesis_Co_Director_fills_annual_report',
-    ]),
-  }
-}
+export const setCoDirectorFillsAttributes =  (taskAttributes: any = generateAGenericTaskAttributes()) => ({
+  ...taskAttributes,
+  'customHeaders': {
+    ...taskAttributes.variables,
+    'title': 'Co-Dir fills annual report - Test',
+  },
+  'variables': {
+    ...taskAttributes.variables,
+    'assigneeSciper': taskAttributes.variables.thesisCoDirectorSciper,
+    'dashboardDefinition': undefined,
+  },
+  'elementId': faker.helpers.arrayElement([
+    'Activity_Thesis_Co_Director_fills_annual_report',
+  ]),
+})
 
-export const getDirectorFills = () => {
-  const taskAttributes= generateAGenericTaskAttributes();
-  return {
-    ...taskAttributes,
-    'customHeaders': {
-      ...taskAttributes.variables,
-      'title': 'Thesis Dir fills annual report - Test',
-    },
-    'variables': {
-      ...taskAttributes.variables,
-      'assigneeSciper': taskAttributes.variables.thesisDirectorSciper,
-      'dashboardDefinition': undefined,
-    },
-    'elementId': faker.helpers.arrayElement([
-      'Activity_Thesis_Director_fills_annual_report',
-    ]),
-  }
-}
+export const setDirectorFillsAttributes = (taskAttributes: any = generateAGenericTaskAttributes()) => ({
+  ...taskAttributes,
+  'customHeaders': {
+    ...taskAttributes.variables,
+    'title': 'Thesis Dir fills annual report - Test',
+  },
+  'variables': {
+    ...taskAttributes.variables,
+    'assigneeSciper': taskAttributes.variables.thesisDirectorSciper,
+    'dashboardDefinition': undefined,
+  },
+  'elementId': faker.helpers.arrayElement([
+    'Activity_Thesis_Director_fills_annual_report',
+  ]),
+})
 
-export const getCollabReview = () => {
-  const taskAttributes= generateAGenericTaskAttributes();
-  return {
-    ...taskAttributes,
-    'customHeaders': {
-      ...taskAttributes.variables,
-      'title': 'Collaborative Review - Test',
-    },
-    'variables': {
-      ...taskAttributes.variables,
-      'assigneeSciper': taskAttributes.variables.thesisDirectorSciper,
-      'dashboardDefinition': undefined,
-    },
-    'elementId': faker.helpers.arrayElement([
-      'Activity_Thesis_Director_Collaborative_Review_Signs',
-    ]),
-  }
-}
+export const setCollabReviewAttributes = (taskAttributes: any = generateAGenericTaskAttributes()) => ({
+  ...taskAttributes,
+  'customHeaders': {
+    ...taskAttributes.variables,
+    'title': 'Collaborative Review - Test',
+  },
+  'variables': {
+    ...taskAttributes.variables,
+    'assigneeSciper': taskAttributes.variables.thesisDirectorSciper,
+    'dashboardDefinition': undefined,
+  },
+  'elementId': faker.helpers.arrayElement([
+    'Activity_Thesis_Director_Collaborative_Review_Signs',
+  ]),
+})
 
-export const getDirectorCollabSign = () => {
-  const taskAttributes= generateAGenericTaskAttributes();
-  return {
-    ...taskAttributes,
-    'customHeaders': {
-      ...taskAttributes.variables,
-      'title': 'Co-Dir signature - Test',
-    },
-    'variables': {
-      ...taskAttributes.variables,
-      'assigneeSciper': taskAttributes.variables.thesisCoDirectorSciper,
-      'dashboardDefinition': undefined,
-    },
-    'elementId': faker.helpers.arrayElement([
-      'Activity_Thesis_Co_Director_Signs',
-    ]),
-  }
-}
+export const setDirectorCollabSignAttributes = (taskAttributes: any = generateAGenericTaskAttributes()) => ({
+  ...taskAttributes,
+  'customHeaders': {
+    ...taskAttributes.variables,
+    'title': 'Co-Dir signature - Test',
+  },
+  'variables': {
+    ...taskAttributes.variables,
+    'assigneeSciper': taskAttributes.variables.thesisCoDirectorSciper,
+    'dashboardDefinition': undefined,
+  },
+  'elementId': faker.helpers.arrayElement([
+    'Activity_Thesis_Co_Director_Signs',
+  ]),
+})
 
-export const getDirectorSigns = () => {
-  const taskAttributes= generateAGenericTaskAttributes();
-  return {
-    ...taskAttributes,
-    'customHeaders': {
-      ...taskAttributes.variables,
-      'title': 'Thesis Dir signature - Test',
-    },
-    'variables': {
-      ...taskAttributes.variables,
-      'assigneeSciper': taskAttributes.variables.thesisDirectorSciper,
-      'dashboardDefinition': undefined,
-    },
-    'elementId': faker.helpers.arrayElement([
-      'Activity_Thesis_Director_Signs',
-    ]),
-  }
-}
+export const setDirectorSignsAttributes = (taskAttributes: any = generateAGenericTaskAttributes()) => ({
+  ...taskAttributes,
+  'customHeaders': {
+    ...taskAttributes.variables,
+    'title': 'Thesis Dir signature - Test',
+  },
+  'variables': {
+    ...taskAttributes.variables,
+    'assigneeSciper': taskAttributes.variables.thesisDirectorSciper,
+    'dashboardDefinition': undefined,
+  },
+  'elementId': faker.helpers.arrayElement([
+    'Activity_Thesis_Director_Signs',
+  ]),
+})
 
-export const getPHDSigns = () => {
-  const taskAttributes= generateAGenericTaskAttributes();
-  return {
-    ...taskAttributes,
-    'customHeaders': {
-      ...taskAttributes.variables,
-      'title': 'PhD signature - Test',
-    },
-    'variables': {
-      ...taskAttributes.variables,
-      'assigneeSciper': taskAttributes.variables.thesisDirectorSciper,
-      'dashboardDefinition': undefined,
-    },
-    'elementId': faker.helpers.arrayElement([
-      'Activity_PHD_Signs',
-    ]),
-  }
-}
+export const setPHDSignsAttributes = (taskAttributes: any = generateAGenericTaskAttributes()) => ({
+  ...taskAttributes,
+  'customHeaders': {
+    ...taskAttributes.variables,
+    'title': 'PhD signature - Test',
+  },
+  'variables': {
+    ...taskAttributes.variables,
+    'assigneeSciper': taskAttributes.variables.thesisDirectorSciper,
+    'dashboardDefinition': undefined,
+  },
+  'elementId': faker.helpers.arrayElement([
+    'Activity_PHD_Signs',
+  ]),
+})
 
-export const getMentorSigns = () => {
-  const taskAttributes= generateAGenericTaskAttributes();
-  return {
-    ...taskAttributes,
-    'customHeaders': {
-      ...taskAttributes.variables,
-      'title': 'Mentor signature - Test',
-    },
-    'variables': {
-      ...taskAttributes.variables,
-      'assigneeSciper': taskAttributes.variables.thesisDirectorSciper,
-      'dashboardDefinition': undefined,
-    },
-    'elementId': faker.helpers.arrayElement([
-      'Activity_Post_Mentor_Meeting_Mentor_Signs',
-    ]),
-  }
-}
+export const setMentorSignsAttributes = (taskAttributes: any = generateAGenericTaskAttributes()) => ({
+  ...taskAttributes,
+  'customHeaders': {
+    ...taskAttributes.variables,
+    'title': 'Mentor signature - Test',
+  },
+  'variables': {
+    ...taskAttributes.variables,
+    'assigneeSciper': taskAttributes.variables.thesisDirectorSciper,
+    'dashboardDefinition': undefined,
+  },
+  'elementId': faker.helpers.arrayElement([
+    'Activity_Post_Mentor_Meeting_Mentor_Signs',
+  ]),
+})
 
-export const getPostMentorPHDSigns = () => {
-  const taskAttributes= generateAGenericTaskAttributes();
-  return {
-    ...taskAttributes,
-    'customHeaders': {
-      ...taskAttributes.variables,
-      'title': 'PhD signature after mentor - Test',
-    },
-    'variables': {
-      ...taskAttributes.variables,
-      'assigneeSciper': taskAttributes.variables.thesisDirectorSciper,
-      'dashboardDefinition': undefined,
-    },
-    'elementId': faker.helpers.arrayElement([
-      'Activity_Post_Mentor_Meeting_PHD_Signs',
-    ]),
-  }
-}
+export const setPostMentorPHDSignsAttributes = (taskAttributes: any = generateAGenericTaskAttributes()) => ({
+  ...taskAttributes,
+  'customHeaders': {
+    ...taskAttributes.variables,
+    'title': 'PhD signature after mentor - Test',
+  },
+  'variables': {
+    ...taskAttributes.variables,
+    'assigneeSciper': taskAttributes.variables.thesisDirectorSciper,
+    'dashboardDefinition': undefined,
+  },
+  'elementId': faker.helpers.arrayElement([
+    'Activity_Post_Mentor_Meeting_PHD_Signs',
+  ]),
+})
 
-export const getProgramDirectorSigns = () => {
-  const taskAttributes= generateAGenericTaskAttributes();
-  return {
-    ...taskAttributes,
-    'customHeaders': {
-      ...taskAttributes.variables,
-      'title': 'Program Director signature - Test',
-    },
-    'variables': {
-      ...taskAttributes.variables,
-      'assigneeSciper': taskAttributes.variables.thesisDirectorSciper,
-      'dashboardDefinition': undefined,
-    },
-    'elementId': faker.helpers.arrayElement([
-      'Activity_Program_Director_Signs',
-    ]),
-  }
-}
+export const setProgramDirectorSignsAttributes = (taskAttributes: any = generateAGenericTaskAttributes()) => ({
+  ...taskAttributes,
+  'customHeaders': {
+    ...taskAttributes.variables,
+    'title': 'Program Director signature - Test',
+  },
+  'variables': {
+    ...taskAttributes.variables,
+    'assigneeSciper': taskAttributes.variables.thesisDirectorSciper,
+    'dashboardDefinition': undefined,
+  },
+  'elementId': faker.helpers.arrayElement([
+    'Activity_Program_Director_Signs',
+  ]),
+})
 
 export const createTasksForDashboardFixtures = () => {
-  Factory.create('task', getPHDFills());
-  Factory.create('task', getCoDirectorFillsAttributes());
-  Factory.create('task', getDirectorFills());
-  Factory.create('task', getCollabReview());
-  Factory.create('task', getDirectorCollabSign());
-  Factory.create('task', getDirectorSigns());
-  Factory.create('task', getPHDSigns());
-  Factory.create('task', getMentorSigns());
-  Factory.create('task', getPostMentorPHDSigns());
-  Factory.create('task', getProgramDirectorSigns());
+  Factory.create('task', setPHDFillsAttributes());
+  Factory.create('task', setCoDirectorFillsAttributes());
+  Factory.create('task', setDirectorFillsAttributes());
+
+  // as we can have multiple task for the same workflow instance (processInstanceKey)
+  // let's build some
+  const oneInstance= generateAGenericTaskAttributes()
+  Factory.create('task', setCoDirectorFillsAttributes(oneInstance));
+  Factory.create('task', setDirectorFillsAttributes(oneInstance));
+
+  Factory.create('task', setCollabReviewAttributes());
+  Factory.create('task', setDirectorCollabSignAttributes());
+  Factory.create('task', setDirectorSignsAttributes());
+  Factory.create('task', setPHDSignsAttributes());
+  Factory.create('task', setMentorSignsAttributes());
+  Factory.create('task', setPostMentorPHDSignsAttributes());
+  Factory.create('task', setProgramDirectorSignsAttributes());
 }
--- apps/fillForm/tests/factories/dashboard/tasksV2.ts
+++ apps/fillForm/tests/factories/dashboard/tasksV2.ts
@@ -1,205 +1,164 @@
 /**
- * Define tasks to expose explicitily steps in the dashboard
+ * Define tasks to expose explicitly steps in the dashboard
  */
-
 import {faker} from "../../factories/faker";
 const Factory = require("meteor/dburles:factory").Factory
 import {generateAGenericTaskAttributes} from "../task";
 import {RethinkedStepsDefinitionV2} from "/imports/ui/components/Dashboard/DefaultDefinition";
 
 
-const getPHDFills1 = () => {
-  const taskAttributes = generateAGenericTaskAttributes();
-  return {
-    ...taskAttributes,
-    'customHeaders': {
-      ...taskAttributes.variables,
-      'title': 'Phd fills annual report - Test',
-    },
-    'variables': {
-      ...taskAttributes.variables,
-      'assigneeSciper': taskAttributes.variables.thesisCoDirectorSciper,
-      'dashboardDefinition': RethinkedStepsDefinitionV2,
-      'doctoralProgramName': 'V2',
-    },
-    'elementId': faker.helpers.arrayElement([
-      'Activity_PHD_fills_annual_report_1',
-    ]),
-  }
-}
+export const setPHDFills1Attributes = (taskAttributes: any = generateAGenericTaskAttributes()) => ({
+  ...taskAttributes,
+  'customHeaders': {
+    ...taskAttributes.variables,
+    'title': 'Phd fills annual report - Test',
+  },
+  'variables': {
+    ...taskAttributes.variables,
+    'assigneeSciper': taskAttributes.variables.thesisCoDirectorSciper,
+    'dashboardDefinition': RethinkedStepsDefinitionV2,
+    'doctoralProgramName': 'V2',
+  },
+  'elementId': faker.helpers.arrayElement([
+    'Activity_PHD_fills_annual_report_1',
+  ]),
+})
 
-const getMentorSigns = () => {
-  const taskAttributes= generateAGenericTaskAttributes();
-  return {
-    ...taskAttributes,
-    'customHeaders': {
-      ...taskAttributes.variables,
-      'title': 'Mentor signature - Test',
-    },
-    'variables': {
-      ...taskAttributes.variables,
-      'assigneeSciper': taskAttributes.variables.thesisDirectorSciper,
-      'dashboardDefinition': RethinkedStepsDefinitionV2,
-      'doctoralProgramName': 'V2',
-    },
-    'elementId': faker.helpers.arrayElement([
-      'Activity_Post_Mentor_Meeting_Mentor_Signs',
-    ]),
-  }
-}
+export const setMentorSignsAttributes = (taskAttributes: any = generateAGenericTaskAttributes()) => ({
+  ...taskAttributes,
+  'customHeaders': {
+    ...taskAttributes.variables,
+    'title': 'Mentor signature - Test',
+  },
+  'variables': {
+    ...taskAttributes.variables,
+    'assigneeSciper': taskAttributes.variables.thesisDirectorSciper,
+    'dashboardDefinition': RethinkedStepsDefinitionV2,
+    'doctoralProgramName': 'V2',
+  },
+  'elementId': faker.helpers.arrayElement([
+    'Activity_Post_Mentor_Meeting_Mentor_Signs',
+  ]),
+})
 
-const getDirectorFills = () => {
-  const taskAttributes= generateAGenericTaskAttributes();
-  return {
-    ...taskAttributes,
-    'customHeaders': {
-      ...taskAttributes.variables,
-      'title': 'Thesis Dir fills annual report - Test',
-    },
-    'variables': {
-      ...taskAttributes.variables,
-      'assigneeSciper': taskAttributes.variables.thesisDirectorSciper,
-      'dashboardDefinition': RethinkedStepsDefinitionV2,
-      'doctoralProgramName': 'V2',
-    },
-    'elementId': faker.helpers.arrayElement([
-      'Activity_Thesis_Director_fills_annual_report',
-    ]),
-  }
-}
+export const setDirectorFillsAttributes = (taskAttributes: any = generateAGenericTaskAttributes()) => ({
+  ...taskAttributes,
+  'customHeaders': {
+    ...taskAttributes.variables,
+    'title': 'Thesis Dir fills annual report - Test',
+  },
+  'variables': {
+    ...taskAttributes.variables,
+    'assigneeSciper': taskAttributes.variables.thesisDirectorSciper,
+    'dashboardDefinition': RethinkedStepsDefinitionV2,
+    'doctoralProgramName': 'V2',
+  },
+  'elementId': faker.helpers.arrayElement([
+    'Activity_Thesis_Director_fills_annual_report',
+  ]),
+})
 
-const getCoDirectorFillsAttributes = () => {
-  const taskAttributes = generateAGenericTaskAttributes();
-  return {
-    ...taskAttributes,
-    'customHeaders': {
-      ...taskAttributes.variables,
-      'title': 'Co-Dir fills annual report - Test',
-    },
-    'variables': {
-      ...taskAttributes.variables,
-      'assigneeSciper': taskAttributes.variables.thesisCoDirectorSciper,
-      'dashboardDefinition': RethinkedStepsDefinitionV2,
-      'doctoralProgramName': 'V2',
-    },
-    'elementId': faker.helpers.arrayElement([
-      'Activity_Thesis_Co_Director_fills_annual_report',
-    ]),
-  }
-}
+export const setCoDirectorFillsAttributes = (taskAttributes: any = generateAGenericTaskAttributes()) => ({
+  ...taskAttributes,
+  'customHeaders': {
+    ...taskAttributes.variables,
+    'title': 'Co-Dir fills annual report - Test',
+  },
+  'variables': {
+    ...taskAttributes.variables,
+    'assigneeSciper': taskAttributes.variables.thesisCoDirectorSciper,
+    'dashboardDefinition': RethinkedStepsDefinitionV2,
+    'doctoralProgramName': 'V2',
+  },
+  'elementId': faker.helpers.arrayElement([
+    'Activity_Thesis_Co_Director_fills_annual_report',
+  ]),
+})
 
-const getPHDFills2 = () => {
-  const taskAttributes = generateAGenericTaskAttributes();
-  return {
-    ...taskAttributes,
-    'customHeaders': {
-      ...taskAttributes.variables,
-      'title': 'Phd fills annual report - Test',
-    },
-    'variables': {
-      ...taskAttributes.variables,
-      'assigneeSciper': taskAttributes.variables.thesisCoDirectorSciper,
-      'dashboardDefinition': RethinkedStepsDefinitionV2,
-      'doctoralProgramName': 'V2',
-    },
-    'elementId': faker.helpers.arrayElement([
-      'Activity_PHD_fills_annual_report_2',
-    ]),
-  }
-}
+export const setPHDFills2Attributes = (taskAttributes: any = generateAGenericTaskAttributes()) => ({
+  ...taskAttributes,
+  'customHeaders': {
+    ...taskAttributes.variables,
+    'title': 'Phd fills annual report - Test',
+  },
+  'variables': {
+    ...taskAttributes.variables,
+    'assigneeSciper': taskAttributes.variables.thesisCoDirectorSciper,
+    'dashboardDefinition': RethinkedStepsDefinitionV2,
+    'doctoralProgramName': 'V2',
+  },
+  'elementId': faker.helpers.arrayElement([
+    'Activity_PHD_fills_annual_report_2',
+  ]),
+})
 
-const getCollabReview = () => {
-  const taskAttributes= generateAGenericTaskAttributes();
-  return {
-    ...taskAttributes,
-    'customHeaders': {
-      ...taskAttributes.variables,
-      'title': 'Collaborative Review - Test',
-    },
-    'variables': {
-      ...taskAttributes.variables,
-      'assigneeSciper': taskAttributes.variables.thesisDirectorSciper,
-      'dashboardDefinition': RethinkedStepsDefinitionV2,
-      'doctoralProgramName': 'V2',
-    },
-    'elementId': faker.helpers.arrayElement([
-      'Activity_Thesis_Director_Collaborative_Review_Signs',
-    ]),
-  }
-}
+export const setCollabReviewAttributes = (taskAttributes: any = generateAGenericTaskAttributes()) => ({
+  ...taskAttributes,
+  'customHeaders': {
+    ...taskAttributes.variables,
+    'title': 'Collaborative Review - Test',
+  },
+  'variables': {
+    ...taskAttributes.variables,
+    'assigneeSciper': taskAttributes.variables.thesisDirectorSciper,
+    'dashboardDefinition': RethinkedStepsDefinitionV2,
+    'doctoralProgramName': 'V2',
+  },
+  'elementId': faker.helpers.arrayElement([
+    'Activity_Thesis_Director_Collaborative_Review_Signs',
+  ]),
+})
 
-const getPHDSigns = () => {
-  const taskAttributes= generateAGenericTaskAttributes();
-  return {
-    ...taskAttributes,
-    'customHeaders': {
-      ...taskAttributes.variables,
-      'title': 'PhD signature - Test',
-    },
-    'variables': {
-      ...taskAttributes.variables,
-      'assigneeSciper': taskAttributes.variables.thesisDirectorSciper,
-      'dashboardDefinition': RethinkedStepsDefinitionV2,
-      'doctoralProgramName': 'V2',
-    },
-    'elementId': faker.helpers.arrayElement([
-      'Activity_PHD_Signs',
-    ]),
-  }
-}
+export const setPHDSignsAttributes = (taskAttributes: any = generateAGenericTaskAttributes()) => ({
+  ...taskAttributes,
+  'customHeaders': {
+    ...taskAttributes.variables,
+    'title': 'PhD signature - Test',
+  },
+  'variables': {
+    ...taskAttributes.variables,
+    'assigneeSciper': taskAttributes.variables.thesisDirectorSciper,
+    'dashboardDefinition': RethinkedStepsDefinitionV2,
+    'doctoralProgramName': 'V2',
+  },
+  'elementId': faker.helpers.arrayElement([
+    'Activity_PHD_Signs',
+  ]),
+})
 
-const getProgramDirectorSigns = () => {
-  const taskAttributes= generateAGenericTaskAttributes();
-  return {
-    ...taskAttributes,
-    'customHeaders': {
-      ...taskAttributes.variables,
-      'title': 'Thesis Dir signature - Test',
-    },
-    'variables': {
-      ...taskAttributes.variables,
-      'assigneeSciper': taskAttributes.variables.thesisDirectorSciper,
-      'dashboardDefinition': RethinkedStepsDefinitionV2,
-      'doctoralProgramName': 'V2',
-    },
-    'elementId': faker.helpers.arrayElement([
-      'Activity_Program_Director_Signs',
-    ]),
-  }
-}
+export const setProgramDirectorSignsAttributes = (taskAttributes: any = generateAGenericTaskAttributes()) => ({
+  ...taskAttributes,
+  'customHeaders': {
+    ...taskAttributes.variables,
+    'title': 'Thesis Dir signature - Test',
+  },
+  'variables': {
+    ...taskAttributes.variables,
+    'assigneeSciper': taskAttributes.variables.thesisDirectorSciper,
+    'dashboardDefinition': RethinkedStepsDefinitionV2,
+    'doctoralProgramName': 'V2',
+  },
+  'elementId': faker.helpers.arrayElement([
+    'Activity_Program_Director_Signs',
+  ]),
+})
 
 export const createTasksForDashboardV2Fixtures = () => {
-  Factory.create('task', getPHDFills1());
-  Factory.create('task', getPHDFills2());
-  Factory.create('task', getMentorSigns());
-  Factory.create('task', getDirectorFills());
-  Factory.create('task', getCoDirectorFillsAttributes());
-  Factory.create('task', getPHDFills2());
-  Factory.create('task', getCollabReview());
-  Factory.create('task', getPHDSigns());
-  Factory.create('task', getProgramDirectorSigns());
+  Factory.create('task', setPHDFills1Attributes());
+  Factory.create('task', setPHDFills2Attributes());
+  Factory.create('task', setMentorSignsAttributes());
+  Factory.create('task', setDirectorFillsAttributes());
+  Factory.create('task', setCoDirectorFillsAttributes());
+  Factory.create('task', setPHDFills2Attributes());
+  Factory.create('task', setCollabReviewAttributes());
+  Factory.create('task', setPHDSignsAttributes());
+  Factory.create('task', setProgramDirectorSignsAttributes());
 
   // as we can have multiple task for the same workflow instance (processInstanceKey)
   // let's build some
-  const processInstanceKey = 1
-
-  Factory.create('task', {
-    ...getPHDFills2(),
-    "processInstanceKey": processInstanceKey
-  });
-
-  Factory.create('task', {
-    ...getMentorSigns(),
-    "processInstanceKey": processInstanceKey
-  });
-
-  Factory.create('task', {
-    ...getDirectorFills(),
-    "processInstanceKey": processInstanceKey
-  });
-
-  Factory.create('task', {
-    ...getCoDirectorFillsAttributes(),
-    "processInstanceKey": processInstanceKey
-  });
+  const oneInstance= generateAGenericTaskAttributes()
+  Factory.create('task', setPHDFills2Attributes(oneInstance));
+  Factory.create('task', setMentorSignsAttributes(oneInstance));
+  Factory.create('task', setDirectorFillsAttributes(oneInstance));
+  Factory.create('task', setCoDirectorFillsAttributes(oneInstance));
 }
