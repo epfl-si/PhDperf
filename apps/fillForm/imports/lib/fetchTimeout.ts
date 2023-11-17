9a8b6402f86332600b2026da6f9f0f0d6e83e6c7
Julien Delasoie <julien.delasoie@epfl.ch>
2023-11-17 09:24:58 +0100

    style: Set better code presentation

--- apps/fillForm/imports/lib/fetchTimeout.ts
+++ apps/fillForm/imports/lib/fetchTimeout.ts
@@ -3,8 +3,20 @@ import { AbortController, AbortSignal } from 'abort-controller'
 
 export const fetchTimeout = (url: string, ms: number, signal: AbortSignal, options = {}) => {
   const controller = new AbortController();
-  const promise = fetch(url, { signal: controller.signal, ...options });
+
+  const promise = fetch(
+    url, {
+      signal: controller.signal,
+      ...options
+    });
+
   if (signal) signal.addEventListener("abort", () => controller.abort());
-  const timeout = setTimeout(() => controller.abort(), ms);
-  return promise.finally(() => clearTimeout(timeout));
+
+  const timeout = setTimeout(
+    () => controller.abort(), ms
+  );
+
+  return promise.finally(
+    () => clearTimeout(timeout)
+  );
 };
